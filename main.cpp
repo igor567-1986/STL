7d831e75715b0cb1c4929c6cc296ffea66062ce1
Petro_qj70@top-academy.ru <Petro_qj70@top-academy.ru>
2022-12-17 21:35:28 +0300

    classWorkEnd

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ https://legacy.cplusplus.com/reference/stl/
 template<typename T>void print(const std::vector<T>& vec);
 
 //#define STL_ARRAY
+//#define MY_EXCEPTION
 #define STL_VECTOR
 
 void main()
@@ -46,7 +47,19 @@ void main()
 	cout << endl;
 #endif // STL_ARRAY
 
+#ifdef MY_EXCEPTION
+	try
+	{
+		throw std::exception("My test exception");
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+#endif // MY_EXCEPTION
+
 #ifdef STL_VECTOR
+
 	//vector - это контейнер, который хранит данные в виде динамического массива.
 	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
 	//std::vector<int> vec(100000);
@@ -69,6 +82,45 @@ void main()
 	//print(vec);
 
 	vec.reserve(12);
+	print(vec);
+
+	try
+	{
+		for (int i = 0; i < vec.capacity(); i++)
+		{
+			//cout << vec[i] << tab;
+			cout << vec.at(i) << tab;
+		}
+		cout << endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	int index;
+	int number;
+	int value;
+	do
+	{
+		cout << "¬ведите индекс добавл€емого элемента: "; cin >> index;
+		if (index >= vec.size())cout << "Out of range" << endl;
+	} while (index >= vec.size());
+	cout << "¬ведите количество добавл€емых значений: "; cin >> number;
+	cout << "¬ведите значение добавл€емого элемента: "; cin >> value;
+
+	vec.insert(vec.begin() + index, number, value);
 	print(vec);
 
 #endif // STL_VECTOR
